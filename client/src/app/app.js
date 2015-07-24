1f11e1dff25a06327c078f6d74217d673ec356ce
Brian Ko <brian.kostar@gmail.com>
2015-07-24 00:00:30 -0400

    get logs

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,7 +1,39 @@
 angular.module('dashKo', ['ngResource', 'lbServices', 'ui.router'])
 
-.config(function (LoopBackResourceProvider) {
-  LoopBackResourceProvider.setUrlBase('https://localhost:3000/api/v1');
-})
+.config(['LoopBackResourceProvider', '$stateProvider', '$urlRouterProvider', function (LoopBackResourceProvider, $stateProvider, $urlRouterProvider) {
+  LoopBackResourceProvider.setUrlBase('http://localhost:3000/api');
 
+    $urlRouterProvider.otherwise("/");
+    //
+    // Now set up the states
+    $stateProvider
+      .state('home', {
+        url: "/",
+        template: "<div>HOME</div>",
+        controller: 'appCtrl'
+      });
+      //.state('state1.list', {
+      //  url: "/list",
+      //  templateUrl: "partials/state1.list.html",
+      //  controller: function($scope) {
+      //    $scope.items = ["A", "List", "Of", "Items"];
+      //  }
+      //})
+  }])
 
+  .factory('lbAPI', ['Log', 'Group', function (Log, Group) {
+    var getLogs = function () {
+      console.log('calling and returning promise..', Log)
+      return Log.find().$promise;
+    };
+    return {
+      getLogs: getLogs
+    }
+  }])
+
+.controller('appCtrl', ['$scope', 'lbAPI', function($scope, lbAPI){
+    console.log('in ctrl')
+    lbAPI.getLogs().then(function(suc){
+      console.log('GOT LOGS', suc)
+    })
+  }]);
