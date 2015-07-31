aa685cfd19dabe3055fbc781b626b845e56704c4
Brian Ko <brian.kostar@gmail.com>
2015-07-30 22:06:39 -0400

    no need for default data

--- server/boot/static-data.js
+++ server/boot/static-data.js
@@ -20,62 +20,62 @@
 //
 //  });
 //};
-
-var async = require('async');
-
-module.exports = function(app) {
-  //data sources
-  var mongoLab = app.dataSources.mongoLab;
-
-  //create all models
-  async.parallel({
-    logs: async.apply(createLogs),
-    groups: async.apply(createGroups)
-  }, function(err, results) {
-    if (err) throw err;
-    //createReviews(results.reviewers, results.coffeeShops, function(err) {
-      console.log('> models created sucessfully');
-    //});
-  });
-
-  //create reviewers
-  function createLogs(cb) {
-    mongoLab.automigrate('Log', function(err) {
-      if (err) return cb(err);
-      var Log = app.models.Log;
-      Log.create([
-        {
-          "date": "2015-07-26T04:12:21.392Z",
-          "value": 1,
-          "groupId": "55b45e253d0f62383e8af029"
-        },
-        {
-          "date": new Date(),
-          "value": 2,
-          "groupId": "55b45e253d0f62383e8af029"
-        }
-      ], cb);
-    });
-  }
-  //create coffee shops
-  function createGroups(cb) {
-    mongoLab.automigrate('Group', function(err) {
-      if (err) return cb(err);
-      var Group = app.models.Group;
-      Group.create([
-        {
-          "id": "55b45e253d0f62383e8af029",
-          "name": "exercise",
-          "description": "Daily exercise in minute",
-          //could potentially be model of its own
-          "unit": "minute"
-        },
-        {
-          "name": "programming",
-          "description": "Programming amount in minute",
-          "unit": "minute"
-        }
-      ], cb);
-    });
-  }
-};
+//
+//var async = require('async');
+//
+//module.exports = function(app) {
+//  //data sources
+//  var mongoLab = app.dataSources.mongoLab;
+//
+//  //create all models
+//  async.parallel({
+//    logs: async.apply(createLogs),
+//    groups: async.apply(createGroups)
+//  }, function(err, results) {
+//    if (err) throw err;
+//    //createReviews(results.reviewers, results.coffeeShops, function(err) {
+//      console.log('> models created sucessfully');
+//    //});
+//  });
+//
+//  //create reviewers
+//  function createLogs(cb) {
+//    mongoLab.automigrate('Log', function(err) {
+//      if (err) return cb(err);
+//      var Log = app.models.Log;
+//      Log.create([
+//        {
+//          "date": "2015-07-26T04:12:21.392Z",
+//          "value": 1,
+//          "groupId": "55b45e253d0f62383e8af029"
+//        },
+//        {
+//          "date": new Date(),
+//          "value": 2,
+//          "groupId": "55b45e253d0f62383e8af029"
+//        }
+//      ], cb);
+//    });
+//  }
+//  //create coffee shops
+//  function createGroups(cb) {
+//    mongoLab.automigrate('Group', function(err) {
+//      if (err) return cb(err);
+//      var Group = app.models.Group;
+//      Group.create([
+//        {
+//          "id": "55b45e253d0f62383e8af029",
+//          "name": "exercise",
+//          "description": "Daily exercise in minute",
+//          //could potentially be model of its own
+//          "unit": "minute"
+//        },
+//        {
+//          "name": "programming",
+//          "description": "Programming amount in minute",
+//          "unit": "minute"
+//        }
+//      ], cb);
+//    });
+//  }
+//};
