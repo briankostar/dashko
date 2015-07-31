c3d052706eab18a09093b0c586aa5e40167b2aeb
Brian Ko <brian.kostar@gmail.com>
2015-07-30 21:57:37 -0400

    highchair draw chart

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -131,46 +131,39 @@ angular.module('dashKo', ['ngResource', 'lbServices', 'ui.router'])
       lbAPI.getGroupLog({groupId: groupId})
         .then(function (suc) {
           console.log('got log for group', suc)
+          drawChart(suc)
         })
     }
 
 
     $scope.getGroups();
 
+    var drawChart = function(logs){
+
+      //get date min and max
+      //get unit min max
+
+      var data = logs.map(function(log){
+        return [log.date, log.value]
+      })
+
+      console.log('data to draw', data)
+
+
+      $('#container').highcharts({
+        xAxis: {
+          type: 'datetime'
+        },
+        series: [{
+          data: data
+        }]
+      });
+    }
+
+
+
   }])
 
 .controller('ChartCtrl', ['$scope', function($scope){
-      $('#container').highcharts({
-        xAxis: {
-          min: -0.5,
-          max: 5.5
-        },
-        yAxis: {
-          min: 0
-        },
-        title: {
-          text: 'Scatter plot with regression line'
-        },
-        series: [{
-          type: 'line',
-          name: 'Regression Line',
-          data: [[0, 1.11], [5, 4.51]],
-          marker: {
-            enabled: false
-          },
-          states: {
-            hover: {
-              lineWidth: 0
-            }
-          },
-          enableMouseTracking: false
-        }, {
-          type: 'scatter',
-          name: 'Observations',
-          data: [1, 1.5, 2.8, 3.5, 3.9, 4.2],
-          marker: {
-            radius: 4
-          }
-        }]
-      });
+
   }])
