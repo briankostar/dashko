9878112b5d0105e2f17431d2f9ead025d7393723
Brian Ko <brian.kostar@gmail.com>
2015-08-03 16:35:57 -0400

    add gulp con

---
+++ gulp/conf.js
@@ -0,0 +1,23 @@
+//write config variables for gulp here
+var gutil = require('gulp-util');
+
+exports.paths = {
+  src: 'src',
+  dist: 'dist',
+  tmp: '.tmp',
+  e2e: 'e2e'
+};
+
+//wiredep inject bower dependencies to index.html and more
+exports.wiredep = {
+  directory: 'bower_components'
+};
+
+exports.errorHandler = function(title) {
+  'use strict';
+
+  return function(err) {
+    gutil.log(gutil.colors.red('[' + title + ']'), err.toString());
+    this.emit('end');
+  };
+};
