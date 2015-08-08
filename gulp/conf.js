4ea9c23b82bcbd52c04e937a310293fdef23fa62
Brian Ko <brian.kostar@gmail.com>
2015-08-07 23:53:33 -0400

    change bower directory

--- gulp/conf.js
+++ gulp/conf.js
@@ -1,16 +1,6 @@
-/**
- *  This file contains the variables used in other gulp files
- *  which defines tasks
- *  By design, we only put there very generic config values
- *  which are used in several places to keep good readability
- *  of the tasks
- */
-
+//write config variables for gulp here
 var gutil = require('gulp-util');
 
-/**
- *  The main paths of your project handle these with care
- */
 exports.paths = {
   src: 'src',
   dist: 'dist',
@@ -18,18 +8,11 @@ exports.paths = {
   e2e: 'e2e'
 };
 
-/**
- *  Wiredep is the lib which inject bower dependencies in your project
- *  Mainly used to inject script tags in the index.html but also used
- *  to inject css preprocessor deps and js files in karma
- */
+//wiredep inject bower dependencies to index.html and more
 exports.wiredep = {
-  directory: 'bower_components'
+  directory: 'client/vendor'
 };
 
-/**
- *  Common implementation for an error handler of a Gulp plugin
- */
 exports.errorHandler = function(title) {
   'use strict';
 
--- gulp/server.js
+++ gulp/server.js
@@ -15,9 +15,10 @@ function browserSyncInit(baseDir, browser) {
   browser = browser === undefined ? 'default' : browser;
 
   var routes = null;
-  if(baseDir === conf.paths.src || (util.isArray(baseDir) && baseDir.indexOf(conf.paths.src) !== -1)) {
+  if (baseDir === conf.paths.src || (util.isArray(baseDir) && baseDir.indexOf(
+      conf.paths.src) !== -1)) {
     routes = {
-      '/bower_components': 'bower_components'
+      '/vendor': '/vendor'
     };
   }
 
@@ -43,21 +44,21 @@ function browserSyncInit(baseDir, browser) {
 }
 
 browserSync.use(browserSyncSpa({
-  selector: '[ng-app]'// Only needed for angular apps
+  selector: '[ng-app]' // Only needed for angular apps
 }));
 
-gulp.task('serve', ['watch'], function () {
+gulp.task('serve', ['watch'], function() {
   browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.src]);
 });
 
-gulp.task('serve:dist', ['build'], function () {
+gulp.task('serve:dist', ['build'], function() {
   browserSyncInit(conf.paths.dist);
 });
 
-gulp.task('serve:e2e', ['inject'], function () {
+gulp.task('serve:e2e', ['inject'], function() {
   browserSyncInit([conf.paths.tmp + '/serve', conf.paths.src], []);
 });
 
-gulp.task('serve:e2e-dist', ['build'], function () {
+gulp.task('serve:e2e-dist', ['build'], function() {
   browserSyncInit(conf.paths.dist, []);
 });
