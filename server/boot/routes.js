538055af566d55ebdd19ffb69bbeccf4055eb55e
Brian Ko <brian.kostar@gmail.com>
2015-08-08 23:52:15 -0400

    route to dist

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app){
   var router = app.loopback.Router();
   router.get('/', function(req, res){
-    res.redirect('src/index.html')
+    res.redirect('dist/index.html')
   });
   app.use(router);
 }
