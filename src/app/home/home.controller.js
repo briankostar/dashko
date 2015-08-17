c12f032acf92439172482549a04a85716124c747
Brian Ko <brian.kostar@gmail.com>
2015-08-17 00:02:49 -0400

    ctrl as before

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -7,12 +7,43 @@
 
   /** @ngInject */
   function HomeController($scope, Group) {
-    console.log('home ctrl activate!')
 
     Group.find().$promise.then(function(suc) {
       console.log('got groups', suc)
-      $scope.group = suc;
+      $scope.groups = suc;
     });
+
+    $scope.getGroupLogs = function(id) {
+      Group.logs({}, {
+        id: id
+      }).$promise.then(function(suc) {
+        console.log('got log for group', suc);
+        drawChart(suc);
+      });
+    };
+
+    var drawChart = function(logs) {
+
+      //get date min and max
+      //get unit min max
+
+      var data = logs.map(function(log) {
+        return [log.date, log.value];
+      });
+
+      console.log('data to draw', data);
+
+
+      $('#container').highcharts({
+        xAxis: {
+          type: 'datetime'
+        },
+        series: [{
+          data: data
+        }]
+      });
+    }
+
 
   }
 })();
